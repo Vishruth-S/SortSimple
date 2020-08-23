3e36832303d2050d2bc40cf7b9b9f140d97778cd
Vishruth S <vs.vishruth.s@gmail.com>
2020-08-24 00:22:57 +0530

    Updates

---
+++ precache-manifest.eac15ff95c4d964cd6217d72436d7cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17b2434d9c02df521dc5dcdbfefb001",
+    "url": "/SortVisual/index.html"
+  },
+  {
+    "revision": "6cf6cad5f98df0b6b290",
+    "url": "/SortVisual/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "648095ad00325345f374",
+    "url": "/SortVisual/static/css/main.7a1c0508.chunk.css"
+  },
+  {
+    "revision": "6cf6cad5f98df0b6b290",
+    "url": "/SortVisual/static/js/2.06c62bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SortVisual/static/js/2.06c62bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648095ad00325345f374",
+    "url": "/SortVisual/static/js/main.6e64a0f4.chunk.js"
+  },
+  {
+    "revision": "ad5db896aa52d14801e3",
+    "url": "/SortVisual/static/js/runtime-main.759182a4.js"
+  }
+]);
